d4ae142088a68e2f773c399b3ba7650b9a1667c5
aki360P <34680805+aki360P@users.noreply.github.com>
2019-10-02 00:38:49 +0900

    reverted

--- db/migrate/001_create_table.rb
+++ db/migrate/001_create_table.rb
@@ -3,7 +3,7 @@ class CreateTable < ActiveRecord::CompatibleLegacyMigration.migration_class
   #for redmine 4x,  class CreateTable < ActiveRecord::Migration[4.2]
 
   def self.up
-    create_table :rmlb_locations, primary_key: :user_id  do |t|
+    create_table :rmlb_locations, id: false, primary_key: :user_id  do |t|
       t.belongs_to :user
       t.column :user_id, :integer
       t.column :user_priority, :integer
